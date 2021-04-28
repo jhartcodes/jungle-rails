c31064cdef4f1d679c4d27d28f3e2a3ba2f5ff73
Joel <joel.d.hart1@gmail.com>
2021-04-28 23:39:02 +0000

    added count for products and catergories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_AUTHOR_USERNAME"], password: ENV["ADMIN_AUTHOR_PASSWORD"]
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
   
 end
